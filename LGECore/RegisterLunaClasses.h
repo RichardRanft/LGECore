64b54efbe00428b6da7dd8a05115a8409d3a4cbc
vegassparkyrich@gmail.com <vegassparkyrich@gmail.com>
2017-04-17 15:43:26 -0700

    Add RegisterLunaClasses.h

---
+++ LGECore/RegisterLunaClasses.h
@@ -0,0 +1,22 @@
+#include "CEvent.h"
+#include "CGuiBase.h"
+#include "CImageResource.h"
+#include "CResourceInstance.h"
+#include "CResourceManager.h"
+#include "CSound.h"
+#include "CSoundResource.h"
+#include "CSurface.h"
+#include "CApp.h"
+
+void registerLuaClasses(lua_State* state)
+{
+	Luna<CEvent>::Register(state);
+	Luna<CGuiBase>::Register(state);
+	Luna<CImageResource>::Register(state);
+	Luna<CResourceInstance>::Register(state);
+	Luna<CResourceManager>::Register(state);
+	Luna<CSound>::Register(state);
+	Luna<CSoundResource>::Register(state);
+	Luna<CSurface>::Register(state);
+	Luna<CApp>::Register(state);
+}
--- LGECore/main.cpp
+++ LGECore/main.cpp
@@ -1,24 +1,5 @@
 #include "stdafx.h"
-#include "LGECore.h"
-#include "CSurface.h"
-#include "CSound.h"
-#include "CResourceManager.h"
-#include "CImageResource.h"
-#include "CSoundResource.h"
-#include "CGuiBase.h"
-
-void registerLuaClasses(lua_State* state)
-{
-	Luna<CApp>::Register(state);
-	Luna<CSound>::Register(state);
-	Luna<CSurface>::Register(state);
-	Luna<CSoundResource>::Register(state);
-	Luna<CImageResource>::Register(state);
-	Luna<CGuiBase>::Register(state);
-	Luna<CEvent>::Register(state);
-	Luna<CResourceManager>::Register(state);
-}
-
+#include "RegisterLunaClasses.h"
 
 int main(int argc, char* argv[]) {
 	lua_State* state = luaL_newstate();
